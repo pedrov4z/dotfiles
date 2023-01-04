cd1dac6b9bb869bfd0471ac2caa6cbbeab1bf1b9
Pedro Vaz <ppvaz03@gmail.com>
2023-01-04 20:46:30 -0300

    feat: add alacritty, nitrogen, p10k, zshrc

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+ln -s $SCRIPTPATH/.alacritty.yml ~/.alacritty.yml
+ln -s $SCRIPTPATH/.p10k.zsh ~/.p10k.zsh
 ln -s $SCRIPTPATH/.vimrc ~/.vimrc
+ln -s $SCRIPTPATH/.zshrc ~/.zshrc
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -10,3 +13,4 @@ ln -s $SCRIPTPATH/.config/nvim ~/.config/nvim
 
 ln -s $SCRIPTPATH/.config/i3 ~/.config/i3
 
+ln -s $SCRIPTPATH/.config/nitrogen ~/.config/nitrogen
